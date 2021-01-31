187debe9f968fbc2ca1b369e97f63f3c08851678
Abdellah <mounajaikabdellah@gmail.com>
2021-01-31 01:18:21 +0100

    exo_fini

---
+++ public/js/main.js
@@ -0,0 +1,40 @@
+// exo1
+
+// let jours = prompt('Le jour de la semaine?').toLowerCase();
+
+// switch (jours) {
+//     case "lundi": case "mardi": case "mercredi" :
+//         console.log(`${jours} MolenGeek`);
+//         break;
+//     case "jeudi": case "vendredi":
+//         console.log(`${jours} elearning`);
+//         break;
+//     case "samedi": case "dimanche":
+//         console.log('maison');
+//         break;
+//     default:
+//         console.log('Erreur, reconnais pas le jour');
+//         break;
+// }
+
+
+// exo2
+
+// let age = parseInt(prompt('Entrez votre âge?'));
+
+// switch (isNaN(age)) {
+//     case true:
+//         console.log(`Error n'est pas un nombre`);
+//         break;
+//     case false:
+//         switch (age >=18 ) {
+//             case true:
+//                 console.log("Il a plus de 18ans");
+//                 break;
+//             case false:
+//                 console.log("ll a moins de 18ans");
+//         }
+//         break;
+// }
+
+
